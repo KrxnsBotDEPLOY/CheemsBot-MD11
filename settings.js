0e7103fdaafed05bbd54c0754bec4e9e3434bcfb
KrxnsBotDEPLOY <kronoshacking07@gmail.com>
2024-11-24 07:39:04 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,12 +16,12 @@ global.sessionid ='ur session id'
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "India, Haiti, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '50938578360' //ur owner number
+global.ownername = '𒁂☠️    𝘒𝘙𝘟𝘕𝘖𝘚!🦇   ⩇⩇:⩇⩇ 🔞' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
@@ -31,7 +31,7 @@ global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["50938578360"] // Premium User
 global.hituet = 0
 
 //bot sett
